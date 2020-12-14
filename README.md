37a8f74f0b731d805bf3671aef79d6329f52294e
fengtianhe <545704061@qq.com>
2020-12-14 15:18:18 +0800

    初版

--- README.md
+++ README.md
@@ -7,5 +7,17 @@ FTH-RBAC 客户端 npm 版
 import FthRbacClient from 'fth-rbac-client';
 
 // http://xxx.xxx.xxx
-FthRbacClient.FthRbacServerDomain = '填入FthRbac搭建服务的地址'
+FthRbacClient.serverDomain = '填入FthRbac搭建服务的地址';
+FthRbacClient.appId = '应用ID';
+
+
+FthRbacClient.menu(roleId).then(resp => {}).catch(e => {});
+FthRbacClient.btn(roleId, pageUrl).then(resp => {}).catch(e => {});
 ```
+### 错误码
+| code | message | 解决方案 |
+| --- | --- | --- |
+| 901 | 未知APPID | 通过FthRbacClient.appId设置 |
+| 902 | 未知应用 | 请检查 appId 和系统中录入的是否一致 |
+| 903 | 未配置应用域 | 请检查系统中录入的domain |
+| 904 | 错误的请求域 | 请检查系统中录入的domain 和 请求的Host是否一致 |
--- index.js
+++ index.js
@@ -1,43 +1,99 @@
+/**
+ *
+ * @param method
+ * @param url
+ * @param params
+ * @returns {Promise<unknown>}
+ */
+function fr(method, url, params) {
+  checkDomain();
+  checkAppId();
+  return new Promise((resolve, reject) => {
+    url = FthRbacClient.serverDomain + url;
+    params['appId'] = FthRbacClient.appId;
+    params['client'] = 'npm';
+
+    if (method === 'GET' || method === 'DELETE') {
+      url = `${url}?${parseParam(params).substring(1)}`;
+    }
+
+    fetch(url, {
+      method: method,
+      body: method === 'POST' || method === 'PUT' ? JSON.stringify(params) : null
+    }).then(function (response) {
+      return response.text();
+    }).then(function (responseText) {
+      const resp = JSON.parse(responseText);
+      if (resp.code !== 200) {
+        console.error(`Fth RBAC Client Error: ${resp.message}`);
+      }
+      resolve(resp);
+    }).catch(e => {
+      reject(e);
+    });
+  });
+}
+
+/**
+ * 检查是否配置FthRbacServer的服务地址
+ */
+function checkDomain() {
+  if (!FthRbacClient.serverDomain) {
+    throw new Error('请检查是否配置FthRbacServer的服务地址，如未设置，请使用FthRbacClient.serverDomain设置');
+  }
+}
+
+/**
+ * 检查是否配置FthRbacServer的AppId
+ */
+function checkAppId() {
+  if (!FthRbacClient.appId) {
+    throw new Error('请检查是否配置FthRbacServer的AppId，如未设置，请使用FthRbacClient.appId设置');
+  }
+}
+
+/**
+ * param 将要转为URL参数字符串的对象
+ * key URL参数字符串的前缀
+ * encode true/false 是否进行URL编码,默认为true
+ *
+ * return URL参数字符串
+ */
+function parseParam(param, key, encode) {
+  if (param == null) return '';
+  var paramStr = '';
+  var t = typeof (param);
+  if (t === 'string' || t === 'number' || t === 'boolean') {
+    paramStr += '&' + key + '=' + ((encode == null || encode) ? encodeURIComponent(param) : param);
+  } else {
+    for (var i in param) {
+      var k = key == null ? i : key + (param instanceof Array ? '[' + i + ']' : '.' + i);
+      paramStr += parseParam(param[i], k, encode);
+    }
+  }
+  return paramStr;
+}
+
+
 const FthRbacClient = {
   serverDomain: null,
 
   appId: null,
   /**
-   * 检查是否配置FthRbacServer的服务地址
-   */
-  checkDomain: function () {
-    if (!this.serverDomain) {
-      throw new Error('请检查是否配置FthRbacServer的服务地址，如未设置，请使用FthRbacClient.serverDomain设置');
-    }
-  },
-  /**
-   * 检查是否配置FthRbacServer的AppId
-   */
-  checkAppId: function () {
-    if (!this.appId) {
-      throw new Error('请检查是否配置FthRbacServer的AppId，如未设置，请使用FthRbacClient.appId设置');
-    }
-  },
-  /**
-   *
+   * 获取目录
    * @param roleId 角色ID，多角色可用 ，分隔
+   * @returns {Promise<unknown>}
    */
   menu: function (roleId) {
-    this.checkDomain();
-    this.checkAppId();
-    return new Promise((resolve, reject) => {
-      const url = `${this.serverDomain}/sdk/resource/menu?roleId=${roleId}&client=npm`;
-      fetch(url, {
-        method: 'GET',
-        credentials: 'include'
-      }).then(function (response) {
-        return response.text();
-      }).then(function (responseText) {
-        resolve(responseText);
-      }).catch(e => {
-        reject(e);
-      });
-    });
+    console.log('FTH RBAC 请求目录数据 ROLEID：', roleId);
+    const url = `/sdk/resource/menu`;
+    return fr('GET', url, { roleId });
+  },
+
+  btn: function (roleId, pageUrl) {
+    console.log('FTH RBAC 请求页面按钮资源 ROLEID：', roleId, 'PAGE: ', pageUrl);
+    const url = `/sdk/resource/btn`;
+    return fr('GET', url, { roleId, pageUrl });
   }
 };
 
